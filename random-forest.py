a0fdaca15c8bfa8a704037f4aa8573f0fda9daf0
niels <nm@9la.dk>
2016-08-13 10:40:14 +0200

    Add classifier tuning step to random forest implementation

--- random-forest.py
+++ random-forest.py
@@ -1,6 +1,7 @@
 import os
+import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn import ensemble, cross_validation
+from sklearn.ensemble import RandomForestClassifier
 from clean import clean_data
 
 
@@ -13,19 +14,39 @@ test = pd.read_csv('data/test.csv')
 clean_data(train)
 clean_data(test)
 features = ['isFemale', 'Age', 'Parch', 'SibSp', 'Pclass', 'Embarked', 'Fare']
+X = train.as_matrix(features)
+Y = train['Survived'].as_matrix()
+
+
+''' TUNE CLASSIFIER '''
+# Initialize different classifiers
+clf = RandomForestClassifier(max_features=None, oob_score=True)
+error_rate = []
+
+# Range of parameter values to explore.
+min_param = 2
+max_param = 20
+params = range(min_param, max_param + 1)
+
+# Run classifiers with increasing max_depth
+for i in params:
+    clf.set_params(n_estimators=50, max_depth=i)
+    clf.fit(X, Y)
+    oob_error = 1 - clf.oob_score_
+    error_rate.append((i, oob_error))
+
+# Plot result
+xs, ys = zip(*error_rate)
+plt.plot(xs, ys)
+plt.xlim(min_param, max_param)
+plt.xlabel("max_depth")
+plt.ylabel("OOB error rate")
+plt.show()
 
 
 ''' TRAIN '''
-X = train.as_matrix(features)
-Y = train['Survived'].as_matrix()
-clf = ensemble.RandomForestClassifier(n_estimators=100)
+clf.set_params(n_estimators=50, max_depth=10)
 clf.fit(X, Y)
-
-
-''' VALIDATE '''
-# Validate using 5-fold stratified cross validation
-scores = cross_validation.cross_val_score(clf, X, Y, cv=5)
-print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
 
 
 ''' TEST '''
@@ -37,4 +58,4 @@ result['Survived'] = clf.predict(test.as_matrix(features))
 ''' OUTPUT '''
 if not os.path.exists('results'):
     os.makedirs('results')
-result.to_csv('results/random-forest3.csv', index=False)
+result.to_csv('results/random-forest4.csv', index=False)
